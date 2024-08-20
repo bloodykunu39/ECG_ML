e02e1113aa0fc1e442df61d8325a8c266af90fb9
Pingal <pratyushnath1610@gmail.com>
2024-08-20 15:35:44 +0530

    CNN model for initial run

--- MAIN/model_cnn.py
+++ MAIN/model_cnn.py
@@ -1,32 +1,51 @@
 "This module contains the CNN model"
 
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class GrayscaleCNN(nn.Module):
+class LargeCNN(nn.Module):
     def __init__(self):
-        super(GrayscaleCNN, self).__init__()
+        super(LargeCNN, self).__init__()
         
-        # Define the CNN architecture for grayscale images
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1)
         
+        # Batch normalization layers
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.bn5 = nn.BatchNorm2d(512)
+        
+        # Pooling layer
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
-        # Calculate the size of the input to the fully connected layer
-        self.fc1 = nn.Linear(128 * 12 * 12, 256)  # 12x12 is from (100/2^3) size after pooling
-        self.fc2 = nn.Linear(256, 3)  # 3 classes
-    
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        # Fully connected layers
+        self.fc1 = nn.Linear(512 * 9 * 9, 1024)
+        self.fc2 = nn.Linear(1024, 3)
         
-        x = x.view(-1, 128 * 12 * 12)  # Flatten the tensor for fully connected layers
+        # Dropout
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        # Convolutional layers with ReLU, batch norm, and pooling
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+        x = self.pool(F.relu(self.bn5(self.conv5(x))))
+        
+        # Flatten the tensor for fully connected layers
+        x = x.view(-1, 512 * 9 * 9)
+        
+        # Fully connected layers with ReLU and dropout
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         
         return x

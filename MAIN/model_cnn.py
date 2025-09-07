1a4e7d1d43204680e4a548ac1d03169137ba58c3
Karan Singh <karankunu51@gmail.com>
2025-09-07 15:35:42 +0530

    CNN for 50*50 added in model_cnn module

--- MAIN/model_cnn.py
+++ MAIN/model_cnn.py
@@ -93,3 +93,39 @@ class SmallCNN(nn.Module):
         x = self.dropout_fc(x)
         x = self.fc2(x)
         return x
+    
+class SmallCNN50(nn.Module):
+    def __init__(self):
+        super(SmallCNN50, self).__init__()
+        
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        
+        # Batch normalization
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        
+        # Pooling
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # Dropout
+        self.dropout_conv = nn.Dropout2d(0.2)
+        self.dropout_fc = nn.Dropout(0.5)
+        
+        # Fully connected layers
+        self.fc1 = nn.Linear(128*6*6, 512)  # Updated for 50x50 input
+        self.fc2 = nn.Linear(512, 3)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.dropout_conv(x)
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = x.view(-1, 128*6*6)  # Updated flatten size
+        x = F.relu(self.fc1(x))
+        x = self.dropout_fc(x)
+        x = self.fc2(x)
+        return x

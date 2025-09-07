953153c9f9814d7cb3c48c83b240d67e1278254a
Karan Singh <karankunu51@gmail.com>
2025-09-07 15:50:34 +0530

    dropout layer added

--- MAIN/model_nn.py
+++ MAIN/model_nn.py
@@ -2,12 +2,15 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 class Model(nn.Module):
     """Base model with 5 layers"""
     def __init__(self, in_features=10000, h1=2000, h2=500,
                  h3=200, h4=50, h5=10,
-                 out_features=3):
+                 out_features=3, dropout_p=0.5):
         """Initialize an instance of the model"""
         super().__init__()
         self.fc1 = nn.Linear(in_features, h1)
@@ -16,14 +19,20 @@ class Model(nn.Module):
         self.fc4 = nn.Linear(h3, h4)
         self.fc5 = nn.Linear(h4, h5)
         self.out = nn.Linear(h5, out_features)
+        self.dropout = nn.Dropout(p=dropout_p)  # Dropout layer
 
     def forward(self, x):
         """Forward method for the layers in the model"""
         # Using softmax in the output layer
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = F.relu(self.fc2(x))
+        x = self.dropout(x)
         x = F.relu(self.fc3(x))
+        x = self.dropout(x)
         x = F.relu(self.fc4(x))
+        x = self.dropout(x)
         x = F.relu(self.fc5(x))
+        x = self.dropout(x)
         x = self.out(x)
         return x
